1eadfae509fa530c1ad6d3753ce5883ae6eb4e5f
nikeshdc-25 <nutternikexdx@gmail.com>
2025-05-31 01:47:12 +0545

    added uploadimage for servers using uploadthing

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModel } from "@/components/models/initial-model";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -18,7 +19,7 @@ const SetupPage = async() => {
     if(server){
         return redirect(`/servers/${server.id}`);
     }
-    return <div>Create a Server</div>;
+    return <InitialModel />;
 }
  
 export default SetupPage;
