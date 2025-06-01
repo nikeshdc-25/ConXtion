ed2161efadbfdfecec96f3aca347839d3a9733a8
nikeshdc-25 <nutternikexdx@gmail.com>
2025-06-01 20:20:20 +0545

    server channel list working

---
+++ components/servers/server-member.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface ServerMemberProps {
+
+}
+
+export const ServerMember = () =>{
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
---
+++ components/servers/server-section.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { ServerWithMembersWithProfiles } from "@/types";
+import { ChannelType, MemberRole } from "@prisma/client";
+import { ActionTooltip } from "../action-tooltip";
+import { Plus, Settings } from "lucide-react";
+import { useModel } from "@/hooks/use-model-store";
+
+interface ServerSectionProps {
+  label: string;
+  role?: string;
+  sectionType: "channels" | "members";
+  channelType?: ChannelType;
+  server?: ServerWithMembersWithProfiles;
+}
+
+export const ServerSection = ({
+  label,
+  role,
+  sectionType,
+  server,
+}: ServerSectionProps) => {
+  const { onOpen } = useModel();
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p className="text-xs font-semibold uppercase text-zinc-500 dark:text-zinc-400">
+        {label}
+      </p>
+      {role !== MemberRole.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("createChannel")}
+            className="text-zinc-500 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+      {role === MemberRole.ADMIN && sectionType === "members" && (
+        <ActionTooltip label="Manage Members" side="top">
+          <button
+            onClick={() => onOpen("members", { server })}
+            className="text-zinc-500 hover:text-zinc-700 dark:text-zinc-400 dark:hover:text-zinc-300"
+          >
+            <Settings className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+    </div>
+  );
+};
